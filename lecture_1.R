362a97b1a93f511fe10134172bb3c844c45397ee
emily-ogden <59844097+emily-ogden@users.noreply.github.com>
2020-01-13 14:02:25 -0500

    First lecture and homework 1

---
+++ lecture_1.R
@@ -0,0 +1,9 @@
+#lecture 1
+#January 7, 2020
+#Emily Ogden
+
+install.packages("knitr")
+library(knitr)
+
+#he wants us to use markdown for our homework assignments
+#github used to save the homework and share with Dr. Gray
